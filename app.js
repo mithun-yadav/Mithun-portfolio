39ed533d8569fb26b6f68111197a53aa5f20d093
Mithun <mithuny1998@gmail.com>
2022-07-15 21:38:50 +0530

    Complet after fixing spacing problem

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ hamburger.addEventListener('click',()=>{
 
 document.addEventListener('scroll',()=>{
     var scroll_position = window.scrollY;
-    if(scroll_position > 250){
+    if(scroll_position > 50){
         header.style.backgroundColor="#29323c";
         // console.log(scroll_position)
     }
