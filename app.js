5ff84baa1f89862c41f3a27d0be4013223e3aa2d
Mithun <mithuny1998@gmail.com>
2023-05-17 11:59:01 +0530

    fully responsive done

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ const menu_item = document.querySelectorAll(".header .nav-bar .nav-list ul li a"
 const header = document.querySelector("#header .header");
 
 hamburger.addEventListener('click',()=>{
-    hamburger.classList.toggle("active"),
-    mobile_menu.classList.toggle("active");
+    hamburger.classList.toggle("active");
+    mobile_menu.classList.toggle("active")
 });
 
 document.addEventListener('scroll',()=>{
     var scroll_position = window.scrollY;
     if(scroll_position > 50){
         header.style.backgroundColor="#29323c";
-        // console.log(scroll_position)
     }
     else{
         header.style.backgroundColor="transparent";
